92918550708e5197e2b2f8e964496d72da27e69e
Pär Hultman <script@hultman.cc>
2017-02-16 07:34:42 +0100

    Nu 44

--- ParFeature.ps1
+++ ParFeature.ps1
@@ -1,2 +1,3 @@
 .\Feb.2017.tests.ps1 -function {[char[]]"$int"|%{('*'*1*[string]$_).padleft(10,'-')}}
 .\Feb.2017.tests.ps1 -function {"$int"[0..9]|%{('*'*1*[string]$_).padleft(10,'-')}}
+.\Feb.2017.tests.ps1 -function {"$int"[0..9]|%{('*'*1*"$_").padleft(10,'-')}}
